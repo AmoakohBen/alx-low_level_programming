6222b413ac7a6f564a1d000146b9d1d5a2eb6b84
Amoakoh <amoakohbenjie@gmail.com>
2022-12-11 18:11:06 -0800

    Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Prints letters of the alphabet
+ *
+ * Return: Always (Success)
+ */
+int main(void) 
+{
+  int ch;
+  for (ch = 'a'; ch <= 'z'; ch++)
+    putchar(ch);
+  putchar('\n');
+  return (0); 
+}
